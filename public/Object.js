7dba8ebd8a9d02a31b3d03bb98bf50056be68baa
BUNROEUN SEREYSARAKROETTH <bunroeun.sereysarakroetth.2822@rupp.edu.kh>
2026-06-18 12:30:36 +0700

    Object and array

---
+++ public/Object.js
@@ -0,0 +1,20 @@
+"use strict";
+// Basic Object 
+Object.defineProperty(exports, "__esModule", { value: true });
+const user = {
+    id: 1,
+    name: "roetth",
+    age: 23,
+};
+// Basic Array 
+const jobs = [
+    "IT",
+    "Cashier",
+    "Teacher"
+];
+// Update Array
+jobs[0] = "IT DEV";
+// Add new Thing 
+jobs.push("police");
+const myArray = ["heng", 24, true];
+//# sourceMappingURL=Object.js.map
---
+++ src/Object.ts
@@ -0,0 +1,35 @@
+// Basic Object 
+
+const user:{
+    id:number,
+    name:string,
+    age:number
+
+    } = {
+    id:1,
+    name:"roetth",
+    age:23,
+};
+
+// Basic Array 
+
+const jobs:string[] = [
+    "IT",
+    "Cashier",
+    "Teacher"
+]
+
+// Update Array
+
+jobs[0] = "IT DEV"
+
+// Add new Thing 
+
+jobs.push("police")
+
+const myArray = ["heng",24,true]
+
+
+
+
+
