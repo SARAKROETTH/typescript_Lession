0c9b354d8b8b0972bce400ee44674a9cef55d37b
BUNROEUN SEREYSARAKROETTH <bunroeun.sereysarakroetth.2822@rupp.edu.kh>
2026-06-18 12:45:03 +0700

    tuple and name tuple

--- public/Tuples.js
+++ public/Tuples.js
@@ -1,3 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const date = [
+    "Apple",
+    1
+];
+// Tuple is the method has been defined the specifix value and type
+//  name tuple
+const student = [
+    1,
+    "Cha cha"
+];
 //# sourceMappingURL=Tuples.js.map
---
+++ src/Tuples.ts
@@ -0,0 +1,16 @@
+const date:[string,number] = [
+    "Apple",
+    1
+]
+
+
+// Tuple is the method has been defined the specifix value and type
+
+//  name tuple
+
+
+const student:[id:number,name:string] =
+[
+    1,
+    "Cha cha"
+] 
